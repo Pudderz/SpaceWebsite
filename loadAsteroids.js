225c9e329bd66edfb6f389a895afe3a251b262e0
Pudderz <mpudney6@gmail.com>
2020-08-25 21:41:46 +0100

    Update loadAsteroids.js
    Can load multiple days easily

--- loadAsteroids.js
+++ loadAsteroids.js
@@ -2,10 +2,17 @@ let api = "2A1UmguNwvSeRTvmHlZ5rXbsFErb3EH8Nu3YPJI2";
 let info;
 let db;
 let ul = document.querySelector('ul');
+let startDate = '';
+let endDate = '';
+let information = document.getElementById('list');
+
 let displayAsteroids = data =>{
     console.log(data);
     
     for(date in data){
+        let newDate = document.createElement('h3');
+        newDate.textContent = date;
+        ul.appendChild(newDate)
         let array = data[date];
         array.forEach(e=>{
             let content = document.createElement('li');
@@ -61,7 +68,6 @@ let displayAsteroids = data =>{
     }
     
 };
-
 
 
 let openRequest = indexedDB.open('storage', 1);
@@ -121,51 +127,49 @@ let removeAsteroid = key =>{
 
 }
 
-let moreDetails = ()=>{
-    let information = document.getElementById('list');
-    information.addEventListener('click', (e)=>{
-        if(e.target.className =="details"){
-            let div = e.target.parentElement.childNodes[1];
-            if(div.style.display == "" || div.style.display == "none"){
-                div.style.display = 'block';    
-            } else{
-                div.style.display = "none";
-            }
-        } else if(e.target.className =="save"){
-            saveAsteroid(e.target.parentElement);
-        } else if (e.target.className =="remove"){
-            removeAsteroid(e.target.parentElement.childNodes[0].textContent);
+
+    
+information.addEventListener('click', element => {
+    if(element.target.className =="details"){
+        let div = element.target.parentElement.childNodes[1];
+    if(div.style.display == "" || div.style.display == "none"){
+            div.style.display = 'block';    
+    } else{
+            div.style.display = "none";
         }
-    })
-};
+    } else if(element.target.className =="save"){
+        saveAsteroid(element.target.parentElement);
+    } else if (element.target.className =="remove"){
+        removeAsteroid(element.target.parentElement.childNodes[0].textContent);
+    }
+})
 
 
 
-function getDate(){
+
+function getDate(i){
     let date = new Date();
-    console.log(date)
-    let day = date.getDate();
-    let year = date.getFullYear();
-    let month = `${date.getMonth()+1}`;
-    if(month.length==1)month = `0${month}`;
-    let photoDate = document.querySelector('#photoDate');
-    photoDate.textContent = `${day}/${month}/${year}`;
-    return `${year}-${month}-${day}`,`${year}-${month}-${day}`;
+    date.setDate(date.getDate()-i)
+    startDate = date.toISOString().split('T')[0];
+    let endingDate = new Date();
+    endingDate.setDate(endingDate.getDate() -i + 1);
+    endDate = endingDate.toISOString().split('T')[0];
 }
 
-let fetchAsteroids = async(startdate,enddate) =>{
-    console.log('running');
-    let url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startdate}&end_date=2020-08-25&api_key=${api}`
+let fetchAsteroids = async(start,end) =>{
+    let url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start}&end_date=${end}&api_key=${api}`;
     fetch(url)
         .then(response => response.json())
         .then(data =>{
-            console.log(data.near_earth_objects);
-            info = data.near_earth_objects;
-            displayAsteroids(info);
+            displayAsteroids(data.near_earth_objects);
             moreDetails();
         })
         .catch(e => console.log(e.error))
 }
 
-
-fetchAsteroids(getDate());
+let i = 0;
+getDate(i);
+fetchAsteroids(startDate, endDate);
+i+=2;
+getDate(i);
+fetchAsteroids(startDate, endDate);
